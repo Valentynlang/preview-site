cea125d34a5b4c1e4baba703bafa870d932cfb1e
Valentynlang <z.dons.v.v@gmail.com>
2025-02-23 18:35:13 +0700

    design fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ export default function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-content">
-        <h1 className="hero-title">Гвоздестояние <span>—</span> <br className="mobile-only"/> путь к освобождению</h1>
-        <p className="hero-description">если ты находишься в поисках себя и выхода из тупика - <br className="style-mobile"/> <span className="mobile-only">я здесь, чтобы тебе в этом помочь</span> <p className="style-mobile">я здесь, чтобы тебе в этом помочь</p></p>
+        <h1 className="hero-title">Гвоздестояние <span>—</span> <br className="style-mobile"/> путь к освобождению</h1>
+        <p className="hero-description">если ты находишься в поисках себя и выхода из тупика - <br className="mobile-only"/> <span className="style-mobile">я здесь, чтобы тебе в этом помочь</span> <p className="display-only">я здесь, чтобы тебе в этом помочь</p></p>
       </div>
     </div>
   );
